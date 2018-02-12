b78ce4666acd44fde32b2338550952db535266cf
Camotubi <camotubi@gmail.com>
2018-02-12 15:49:56 -0500

    aun no se que hice

--- app/Jobs/FetchAleksApi.php
+++ app/Jobs/FetchAleksApi.php
@@ -42,8 +42,8 @@ class FetchAleksApi implements ShouldQueue
             'http'=>array(
                 'method'=>"GET",
                 'header'=>"Accept-language: en\r\n" .
-                "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
-                "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+                "Cookie: foo=bar\r\n" .
+                "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" 
             )
         );
         $context = stream_context_create($options);
