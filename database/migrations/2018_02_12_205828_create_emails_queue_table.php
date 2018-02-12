5ebeab83bc607bb28c13a6a4984053dc6cfab27f
Camotubi <camotubi@gmail.com>
2018-02-12 16:15:36 -0500

    add create_emails_queue_table migrations to deal with the gmail 2000 emails limit

---
+++ database/migrations/2018_02_12_205828_create_emails_queue_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails_queue', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('student_id');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->string('email_type');
+            $table->date('execution_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails_queue');
+    }
+}
