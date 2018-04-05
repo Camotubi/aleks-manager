ad0ccd497ec266f6f390c55f0ba01ad02f842696
Carlos Moisés Tuñón Biddy <camotubi@gmail.com>
2018-04-04 19:06:22 -0500

    fix classs name of NoProgress file

--- app/Mail/NoProgress.php
+++ app/Mail/NoProgress.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class Felicitate extends Mailable
+class NoProgress extends Mailable
 {
     use Queueable, SerializesModels;
 
