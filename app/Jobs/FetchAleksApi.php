bafef4e996067e8938d14d0d9de84aecc28507ce
Carlos Moisés Tuñón Biddy <camotubi@gmail.com>
2018-04-18 19:03:11 -0500

    fix fetch aleks api job not pulling all pages

--- app/Jobs/FetchAleksApi.php
+++ app/Jobs/FetchAleksApi.php
@@ -41,7 +41,6 @@ class FetchAleksApi implements ShouldQueue
 		$cohorts = Cohort::all();
 		foreach($cohorts as $cohort)
 		{
-			$aleksUrl = "https://api.aleks.com/urlrpc?method=getPlacementReport&username={$aleksUser}&password={$aleksPass}&class_code={$cohort->class_code}&type=all&show_prep=true";
 			$options = array(
 				'http'=>array(
 					'method'=>"GET",
@@ -50,8 +49,20 @@ class FetchAleksApi implements ShouldQueue
 					"User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" 
 				)
 			);
-			$context = stream_context_create($options);
-			$studentsPlacements = (new \parseCSV(file_get_contents($aleksUrl,false,stream_context_create($options))))->data;
+            $context = stream_context_create($options);
+            $page_num = 1;
+            $studentsPlacements = [];
+            do{
+                $aleksUrl = "https://api.aleks.com/urlrpc?method=getPlacementReport&username={$aleksUser}&password={$aleksPass}&class_code={$cohort->class_code}&type=all&show_prep=true&page_num={$page_num}";
+
+                $response =file_get_contents($aleksUrl,false,stream_context_create($options));
+                if(trim($response) != "No records found")
+                {
+                    $studentsPlacements = array_merge($studentsPlacements,(new \parseCSV($response))->data);
+                    $page_num++;
+                }
+            }while(trim($response) != "No records found");
+
 			foreach($studentsPlacements as $studentPlacement)
 			{
 				$student = Student::find($studentPlacement["Student Id"]);	
