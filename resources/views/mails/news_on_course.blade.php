d61b6d7dc4d50a413324a8a75fef39f59160dbdc
Carlos Tuñón <camotubi@gmail.com>
2018-08-14 13:00:38 -0500

    Fix typo on word mas to más

--- resources/views/mails/news_on_course.blade.php
+++ resources/views/mails/news_on_course.blade.php
@@ -7,7 +7,7 @@
 					<td>
 						<div class="mail-content column ">
 							<p>Hola <strong>@if(isset($student->first_name)){{$student->first_name}}@else nombre @endif</strong>,</p>
-							<p>Queríamos darte la noticia de que si te aplicas en la plataforma ALEKS PPL podras entrar a clases en la UTP en marzo y no en enero. De esta manera podrás disfrutar mas tu verano <span style="font-size:20px;">🤓</span>.</p>
+							<p>Queríamos darte la noticia de que si te aplicas en la plataforma ALEKS PPL podras entrar a clases en la UTP en marzo y no en enero. De esta manera podrás disfrutar más tu verano <span style="font-size:20px;">🤓</span>.</p>
                             <img width="100%" src="data:image/png;base64,{{base64_encode(file_get_contents(base_path('public/img/aleks_skip_verano.jpg')))}}" alt="">
 
 							<p>Atentamente,</p>
